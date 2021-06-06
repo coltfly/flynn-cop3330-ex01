3900671c6d43a9bc54fd2d8123ac703cc9982f0b
coltfly <coltfly@gmail.com>
2021-06-06 18:43:21 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Colton Flynn
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner Obj = new Scanner(System.in);
+        System.out.print( "What is your name? ");
+        String name = Obj.nextLine();
+        System.out.print("Hello, " + name);
+        System.out.print(", nice to meet you!");
+    }
+}
